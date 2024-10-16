658a08598bc6c50ec4422eb2c314c811f3946d63
DanRoscigno <dan@roscigno.com>
2024-10-16 10:03:17 -0400

    add command
    Signed-off-by: DanRoscigno <dan@roscigno.com>

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # scrapy-tutorial
+
+Goal is to detect redirects and 404's 
+
+By default, scrapy misses `meta refresh` redirects, although they show in the debug log:
+
+```bash
+[scrapy.downloadermiddlewares.redirect]
+DEBUG: Redirecting (meta refresh) to <GET https://newURL/> from <GET https://oldURL>
+```
+
+This
+1. cleans up the output and log
+2. checks the links in `doc_urls.txt` (hardcoded filename)
+3. generates a CSV file named `x.csv` with any redirected pages
+4. pops the debug log in a file named `log`
+
+```bash
+rm x.csv log && scrapy crawl quotes -o x.csv >log  2>&1
+```
--- tutorial/spiders/quotes_spider.py
+++ tutorial/spiders/quotes_spider.py
@@ -1,10 +1,17 @@
 from pathlib import Path
+from scrapy.item import Item, Field
 
 import scrapy
 
+class MyItems(Item):
+    referer = Field()  # where the link is extracted
+    response = Field()  # url that was requested
+    status = Field()  # status code received
 
 class QuotesSpider(scrapy.Spider):
     name = "quotes"
+    handle_httpstatus_list = [301,302,400,404,500]
+    handle_httpstatus_all = True
 
     def start_requests(self):
         with open('doc_urls.txt') as f:
@@ -17,8 +24,16 @@ class QuotesSpider(scrapy.Spider):
             yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
-        page = response.url.split("/")[-2]
+        report_if = [301,302,400,404,500]
+        #page = response.url.split("/")[-2]
         #filename = f"quotes-{page}.html"
         #Path(filename).write_bytes(response.body)
-        self.log(f"checked URL {page}")
+        #self.log(f"checked URL {page}")
+        if response.status in report_if:  # if the response matches then creates a MyItem
+            item = MyItems()
+            item["referer"] = response.request.headers.get("Referer", None)
+            item["status"] = response.status
+            item["response"] = response.url
+            yield item
+        yield None  # return empty if response didn't match a code in `report_if`
 
